17915f0176866c8b3c1923facd3cb728220f2c8f
voruti <45130096+voruti@users.noreply.github.com>
2021-04-03 18:45:20 +0200

    Add Ping-Pong functionality

--- src/main/java/voruti/discordlanguagegaming/listener/DiscordListenerAdapter.java
+++ src/main/java/voruti/discordlanguagegaming/listener/DiscordListenerAdapter.java
@@ -1,5 +1,7 @@
 package voruti.discordlanguagegaming.listener;
 
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -13,5 +15,15 @@ public class DiscordListenerAdapter extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         LOGGER.info("Generous message: {}", event.getMessage());
+
+        Message msg = event.getMessage();
+        if (msg.getContentRaw().equals("!ping")) {
+            MessageChannel channel = event.getChannel();
+            long time = System.currentTimeMillis();
+            channel.sendMessage("Pong!") /* => RestAction<Message> */
+                    .queue(response /* => Message */ -> {
+                        response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
+                    });
+        }
     }
 }
