08a1750cad9da523e11a13598f4ae7b17f92b978
voruti <45130096+voruti@users.noreply.github.com>
2021-04-04 14:52:04 +0200

    Add StaticMethods

--- src/main/java/voruti/discordlanguagegaming/game/NoAuthor.java
+++ src/main/java/voruti/discordlanguagegaming/game/NoAuthor.java
@@ -8,12 +8,13 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import voruti.discordlanguagegaming.utility.Constants;
+import voruti.discordlanguagegaming.utility.StaticMethods;
 
 public class NoAuthor extends ListenerAdapter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NoAuthor.class);
 
-    private long botId;
+    private final long botId;
 
     public NoAuthor(long botId) {
         this.botId = botId;
@@ -33,7 +34,7 @@ public class NoAuthor extends ListenerAdapter {
             channel.sendMessage(msg).queue();
 
             // respond with help text, if requested:
-            if (msg.getContentRaw().contains(Constants.HELP_CMD)) {
+            if (StaticMethods.messageMatchesCmd(msg, Constants.HELP_CMD)) {
                 channel.sendMessage("Man wei\u00DF halt einfach nicht, von wem die Nachricht kommt.").queue();
             }
         }
--- src/main/java/voruti/discordlanguagegaming/utility/Constants.java
+++ src/main/java/voruti/discordlanguagegaming/utility/Constants.java
@@ -1,8 +1,15 @@
 package voruti.discordlanguagegaming.utility;
 
+import java.util.Arrays;
+import java.util.List;
+
 public final class Constants {
 
-    public static final String HELP_CMD = "!was";
+    public static final String CMD_CHAR = "!";
+    public static final List<String> HELP_CMD = Arrays.asList(
+            CMD_CHAR + "was", CMD_CHAR + "what",
+            CMD_CHAR + "hilfe", CMD_CHAR + "help"
+    );
 
     private Constants() {
     }
