4d73278d0c097d93306fcc054c1d5b1da366ed20
voruti <45130096+voruti@users.noreply.github.com>
2021-04-04 14:40:26 +0200

    Finish first game NoAuthor

--- src/main/java/voruti/discordlanguagegaming/Main.java
+++ src/main/java/voruti/discordlanguagegaming/Main.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import voruti.discordlanguagegaming.game.DiscordListenerAdapter;
+import voruti.discordlanguagegaming.game.NoAuthor;
 
 import javax.security.auth.login.LoginException;
 
@@ -12,19 +12,23 @@ public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws InterruptedException {
+    public Main() throws InterruptedException {
         LOGGER.info("Started");
 
         String token = System.getenv("TOKEN");
 
         try {
             JDA jda = JDABuilder.createDefault(token)
-                    .addEventListeners(new DiscordListenerAdapter())
                     .build();
-
             jda.awaitReady();
+
+            jda.addEventListener(new NoAuthor(jda.getSelfUser().getIdLong()));
         } catch (LoginException | IllegalArgumentException e) {
             LOGGER.error("Exception occurred", e);
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new Main();
     }
 }
--- src/main/java/voruti/discordlanguagegaming/game/NoAuthor.java
+++ src/main/java/voruti/discordlanguagegaming/game/NoAuthor.java
@@ -2,27 +2,36 @@ package voruti.discordlanguagegaming.game;
 
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DiscordListenerAdapter extends ListenerAdapter {
+public class NoAuthor extends ListenerAdapter {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(DiscordListenerAdapter.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(NoAuthor.class);
+
+    private long botId;
+
+    public NoAuthor(long botId) {
+        this.botId = botId;
+    }
 
     @Override
-    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        if (!event.getAuthor().isBot()) {
-            LOGGER.info("Generous message: {}", event.getMessage());
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        if (event.getAuthor().getIdLong() != botId) {
+            LOGGER.debug("Received message: {}", event.getMessage());
 
+            // delete received message:
             Message msg = event.getMessage();
             msg.delete().queue();
 
+            // send same message again:
             MessageChannel channel = event.getChannel();
             channel.sendMessage(msg).queue();
 
+            // respond with help text, if requested:
             if (msg.getContentRaw().contains("!was")) {
                 channel.sendMessage("Man wei\u00DF halt einfach nicht, von wem die Nachricht kommt.").queue();
             }
